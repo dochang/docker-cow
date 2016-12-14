cd648e6b768b94939b10527985da33847d7764f0
Desmond O. Chang <dochang@gmail.com>
2016-12-15 00:05:11 +0800

    Fix build script

--- scripts/cow/install.sh
+++ scripts/cow/install.sh
@@ -14,12 +14,12 @@ mkdir -p "${GOPATH}/src" "${GOPATH}/bin"
 pkgroot=github.com/cyfdecyf/cow
 git clone --branch "${COW_VERSION}" https://${pkgroot}.git "${GOPATH}/src/${pkgroot}"
 export CGO_ENABLED=0
-go get -a -installsuffix nocgo ${pkgroot}
+go get ${pkgroot}
 
 cd "${GOPATH}/bin"
 install -c cow /usr/local/bin
 cd /
 
-rm -rf "${GOPATH}" /usr/local/go/pkg/linux_amd64_nocgo
+rm -rf "${GOPATH}"
 
 apk del --purge cow-dependencies
